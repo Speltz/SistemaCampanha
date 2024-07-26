104b00b90a7860243a966013a8a643a68658b38b
Speltz <gespeltz@gmail.com>
2024-07-26 10:03:48 -0300

    Edit, Delete

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
+const mysql = require('mysql2');
 const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Conexão do banco de dados
-mongoose.connect(process.env.DB_URI);
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Conectado ao banco de dados"));
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
 
-//Middlewares
-app.use(express.urlencoded({extended: false}));
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados: ' + err.stack);
+        return;
+    }
+    console.log('Conectado ao banco de dados como id ' + connection.threadId);
+});
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(session({
     secret: 'chave secreta',
     saveUninitialized: true,
     resave: false,
-})
-);
+}));
 
-app.use((req, res, next) => {res.locals.message = req.session.message;
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
     delete req.session.message;
     next();
 });
 
-//Definir template engine
+// Definir template engine
 app.set('view engine', 'ejs');
 
-//Prefixo de rota
-app.use("", require("./routes/routes"));
+// Prefixo de rota
+const routes = require('./routes/routes')(connection);
+app.use("", routes);
 
 // app.get('/', (req, res) => {
 //     res.send("Hello World");
@@ -40,4 +51,4 @@ app.use("", require("./routes/routes"));
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
-})
+});
